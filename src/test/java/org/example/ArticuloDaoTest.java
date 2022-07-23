a98992d4995802a2ebf191a1e7eaf7c0e01d70ef
ampinkus <ampinkus@gmail.com>
2022-07-23 17:07:20 -0300

    testeos save y delete completados

--- src/test/java/org/example/ArticuloDaoTest.java
+++ src/test/java/org/example/ArticuloDaoTest.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
-import static org.junit.Assert.assertEquals;
-
 
 class ArticuloDaoTest {
 
-    @BeforeEach
+    @BeforeEach  // ejecuto antes de cada testeo
     void setUp() {
         Database db = Database.instance(); // creo la instancia db de la clase Database
         try { // me conecto a la base de datos
@@ -21,7 +19,7 @@ class ArticuloDaoTest {
         }
     }
 
-    @AfterEach
+    @AfterEach  // ejecuto despues de cada testeo
     void tearDown() {
         Database db = Database.instance(); // creo la instancia db de la clase Database
         try { // al final cierro la conexion con la base de datos
@@ -33,7 +31,8 @@ class ArticuloDaoTest {
     }
 
     @Test
-    void save() throws SQLException {
+    void save() throws SQLException { // pruebo si el metodo save() gurda un valor de forma adecuada
+        // given: Donde ponemos los datos
         // creo una conexión con la base de datos
         var conn = Database.instance().getConnection();
         // creo una instancia articuloDao para poder usar los métodos de la clase
@@ -45,6 +44,7 @@ class ArticuloDaoTest {
         // guardo el artículo en la base de datos
         articuloDao.save(articulo);
 
+        //when: Donde ejecutamos el método a probar
         // traigo el artículo de la base de datos
         // preparo una variable que guarde un query
         var stmt = conn.createStatement();
@@ -53,18 +53,46 @@ class ArticuloDaoTest {
         // es el ultimo query
         var result = rs.next();
 
-        // testeo el resultado:
-
+        // then: Donde chequeamos el resultado
         // si no puedo recuperar el valor de la base de datos genero un error
         // si hay un error result es falso
         Assertions.assertTrue(result, "No puedo recuperar el artículo insertado");
-        // recupero el nombre dle rtículo que tenía índice 100 (cucharon)
+        // recupero el nombre del artículo que tenía índice 100 (cucharon)
         var name = rs.getString("nombre");
         // comparo el nombre del artículo que guarde con el que recuperé de la base de datos
         Assertions.assertEquals(articulo.getNombre(), name, "El nombre del articulo no coincide con el guardado");
 
-
         stmt.close();
 
+        }
+
+    @Test
+    void delete() throws SQLException{
+        // given: Donde ponemos los datos
+        // creo una conexión con la base de datos
+        var conn = Database.instance().getConnection();
+        // creo una instancia articuloDao para poder usar los métodos de la clase
+        ArticuloDao articuloDao = new ArticuloDao();
+        // limpio la base de datos antes de comenzar
+        articuloDao.deleteAll();
+        // creo un artículo
+        Articulo articulo = new Articulo(100,"cucharon","1200");
+        // guardo el artículo en la base de datos
+        articuloDao.save(articulo);
+
+        //when: Donde ejecutamos el método a probar
+        // borro el artículo recien guardado
+        articuloDao.delete(100);
+        // preparo un statement para traer el artículo de la base de datos
+        var stmt = conn.createStatement();
+        // ejecuto el query y guardo el resultado en rs
+        var rs = stmt.executeQuery("select nombre from bazartest.articulo where id = 100");
+        // es el ultimo query
+        var result = rs.next();
+
+        // then: Donde chequeamos el resultado
+        // si aún está alli es que no fue borrado
+        Assertions.assertFalse(result, "Error el objeto sigue en la base de datos");
     }
+    
 }
